1744ac1cffa7f4cb1e68daecadcbedd4b4d582a7
Pavel <flyguy.by@gmail.com>
2014-06-04 00:48:57 +0300

    bug-fix

--- src/types/DateTime.php
+++ src/types/DateTime.php
@@ -4,14 +4,14 @@ namespace declarativeForms\types;
 class DateTime extends \DateTime {
     protected $format = 'Y-m-d';
 
-    public function __construct($time='now', \DateTimeZone $timezone=null, $format=null) {
+    public function __construct($time='now', $timezone=null, $format=null) {
         if($format) {
             $this->format = $format;
         }
         parent::__construct($time, $timezone);
     }
 
-    public static function createFromFormat($format, $time, \DateTimeZone $timezone=null) {
+    public static function createFromFormat($format, $time, $timezone=null) {
         $datetime = parent::createFromFormat($format, $time, $timezone);
         $datetime->format = $format;
         return $datetime;
