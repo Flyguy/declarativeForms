bea95a8d60610cc655a90162367fefddda5cfe77
Pavel <flyguy.by@gmail.com>
2014-06-07 02:19:35 +0300

    Bug-fix

--- src/fields/Boolean.php
+++ src/fields/Boolean.php
@@ -3,7 +3,6 @@ namespace declarativeForms\fields;
 
 class Boolean extends Text {
     protected $type = 'checkbox';
-    protected $default = "1";
 
     protected function process_default($default) {
         return !!$default;
@@ -15,11 +14,15 @@ class Boolean extends Text {
         }
         return false;
     }
-
     public function toString(array $custom_props=Array()) {
+        $args = Array(
+            'type' => $this->type,
+            'name' => $this->name(),
+            'id' => $this->id(),
+        );
         if(!isset($custom_props['checked']) && $this->form_data(true)) {
             $custom_props['checked'] = 'checked';
         }
-        return parent::toString($custom_props);
+        return '<input '.static::render_attributes($args+$custom_props).' value="1"/>';
     }
 }
